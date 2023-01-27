9dc47d6ece5f80459bcea17593f3aaddd4a7ac71
Gabor Szabo <gabor@szabgab.com>
2023-01-27 23:26:38 +0200

    github_actor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
-#!/bin/sh -le
+#!/usr/bin/bash -le
 
 root=$(dirname $0)
+printenv | sort
 
 pytest -svx $root/test_json.py
 # pytest -svx $root/test_urls.py
-python $root/generate.py
+if [ "$GITHUB_ACTOR" == "" ] || [ "$GITHUB_ACTOR" == "osdc-code-maven" ];
+then
+    echo "run $root/generate.py"
+    python $root/generate.py
+fi
