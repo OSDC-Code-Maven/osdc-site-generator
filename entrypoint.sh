c422383c9d24618b579e4cb0d612fa48fe5dc38d
Gabor Szabo <gabor@szabgab.com>
2023-01-27 23:30:33 +0200

    experiment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/bash -le
+#!/bin/sh -le
 
 root=$(dirname $0)
 printenv | sort
+ls -l /usr/bin/
+which bash
 
-pytest -svx $root/test_json.py
-# pytest -svx $root/test_urls.py
-if [ "$GITHUB_ACTOR" == "" ] || [ "$GITHUB_ACTOR" == "osdc-code-maven" ];
-then
-    echo "run $root/generate.py"
-    python $root/generate.py
-fi
+#pytest -svx $root/test_json.py
+## pytest -svx $root/test_urls.py
+#if [ "$GITHUB_ACTOR" == "" ] || [ "$GITHUB_ACTOR" == "osdc-code-maven" ];
+#then
+#    echo "run $root/generate.py"
+#    python $root/generate.py
+#fi
