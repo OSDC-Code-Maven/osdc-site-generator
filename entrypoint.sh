00fc38af8c1ef8b4f8ea3fa7dde97530271a3328
Gabor Szabo <gabor@szabgab.com>
2023-04-17 17:27:26 +0300

    always run the generate

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,9 @@ echo "GITHUB_ACTOR=$GITHUB_ACTOR"
 # if [ "$GITHUB_ACTOR" == "" ] || [ "$GITHUB_ACTOR" == "szabgab" ];
 
 echo "GITHUB_EVENT_NAME=$GITHUB_EVENT_NAME"
-if [ "$GITHUB_EVENT_NAME" != "pull_request" ];
-then
-    echo "not a pull-request so we run $root/generate.py"
-    python $root/generate.py
-fi
+# if [ "$GITHUB_EVENT_NAME" != "pull_request" ];
+# then
+#     echo "not a pull-request so we run $root/generate.py"
+#     python $root/generate.py
+# fi
+python $root/generate.py
